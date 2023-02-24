8c48a473792c435137e6b22a2f0ed8287d0dcb8e
kaguithiajohnson@gmail.com <kaguithiajohnson@gmail.com>
2023-02-24 18:51:19 +0300

    attribute-binding, computed properties

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
  const app = Vue.createApp({
      data() {
          return {
+             url: "https://jkaberere-portfolio.netlify.app/",
              showBooks: true,
-             title: "The final Empire",
-             author: 'Brandon Sanderson',
-             age: 40,
-             x: 0,
-             y: 0
-
+             books: [{ title: 'name of the wind', author: 'patrick rothfus', img: 'assets/1.jpg', isFav: true },
+                 { title: 'the way of the kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
+                 { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true }
+             ],
          }
      },
      methods: {
          toggleShowBooks() {
              this.showBooks = !this.showBooks
          },
-         handleEvent(e) {
-             console.log(e, e.type)
-
-         },
-         handleMousemove(e) {
-             this.x = e.offsetX
-             this.y = e.offsetY
+         toggleFaveBook(book) {
+             book.isFav = !book.isFav
 
          }
-
+     },
+     computed: {
+         filteredBooks() {
+             return this.books.filter((book) => book.isFav)
+         }
      }
  })
 
